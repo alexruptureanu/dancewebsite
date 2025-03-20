b0ff2e96479584e4e3954511f74fb3c9e0719df0
Alexandru RUPTUREANU (108801) <aruptureanu@stud.etti.upb.ro>
2025-03-20 19:28:05 +0200

    da

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<div class="footer-area footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <!-- Copywrite Text -->
+                <div class="copywrite-text text-center">
+                    <p>Contesina Dance Festival &copy;</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
